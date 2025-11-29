69cf23c4987230bdfffff587568ce0fd0b311853
goawwer <onlyforprogrammingpost@gmail.com>
2025-11-29 11:39:02 +0500

    add defaults in tech_stacks table

--- backend/migrations/000003_tech_stacks_table.up.sql
+++ backend/migrations/000003_tech_stacks_table.up.sql
@@ -2,3 +2,26 @@ CREATE TABLE tech_stacks (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     name VARCHAR(150) NOT NULL UNIQUE
 );
+
+INSERT INTO
+    tech_stacks (name)
+VALUES ('Go'),
+    ('React'),
+    ('Next.js'),
+    ('C'),
+    ('JavaScript'),
+    ('TypeScript'),
+    ('Node.js'),
+    ('Python'),
+    ('Java'),
+    ('C#'),
+    ('C++'),
+    ('Rust'),
+    ('PHP'),
+    ('Ruby'),
+    ('Swift'),
+    ('Kotlin'),
+    ('Dart'),
+    ('Angular'),
+    ('Vue')
+ON CONFLICT (name) DO NOTHING;
